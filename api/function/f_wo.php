e873628aed2a7d56de1b2d1ddc54cd5242c491a3
zaid1982 <hemppok.kembong@gmail.com>
2019-08-31 02:11:57 +0800

    API WO Technician Revisit - don't reset info in assigner's screen

--- api/function/f_wo.php
+++ api/function/f_wo.php
@@ -1055,8 +1055,8 @@ class Class_wo {
             if ($woTask['transaction_id'] !== $transactionId) {
                 throw new Exception('[' . __LINE__ . '] - Parameter transactionId invalid');
             }
-            Class_db::getInstance()->db_update('wo_task', array('wo_task_assigned_by'=>'', 'wo_task_time_assigned'=>'',  'wo_task_status'=>'24'), array('wo_task_id'=>$this->woTaskId));
-            Class_db::getInstance()->db_update('wfl_transaction', array('transaction_status'=>'24'), array('transaction_id'=>$transactionId));
+            Class_db::getInstance()->db_update('wo_task', array('wo_task_assigned_by'=>'', 'wo_task_time_assigned'=>'',  'wo_task_status'=>'26'), array('wo_task_id'=>$this->woTaskId));
+            Class_db::getInstance()->db_update('wfl_transaction', array('transaction_status'=>'26'), array('transaction_id'=>$transactionId));
 
             return array(
                 'woTaskNo'=>$woTask['wo_task_no'],
