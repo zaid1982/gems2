185aff314a3bbd62fcb6aff9fda33a6ffe69b224
zaid1982 <hemppok.kembong@gmail.com>
2020-02-22 03:16:33 +0800

    Create jai_batch_assign.php

---
+++ api/jai_batch_assign.php
@@ -0,0 +1,49 @@
+<?php
+
+ini_set('max_execution_time', 0);
+
+require_once 'library/constant.php';
+require_once 'function/db.php';
+require_once 'function/f_general.php';
+require_once 'function/f_email.php';
+require_once 'function/f_task.php';
+require_once 'function/f_ppm.php';
+
+$api_name = 'backdoor_assign';
+$is_transaction = false;
+
+$constant = new Class_constant();
+$fn_general = new Class_general();
+$fn_task = new Class_task();
+$fn_email = new Class_email();
+$fn_ppm = new Class_ppm();
+
+try {
+    $fn_email->__set('fn_general', $fn_general);
+    $fn_task->__set('constant', $constant);
+    $fn_task->__set('fn_general', $fn_general);
+    $fn_ppm->__set('constant', $constant);
+    $fn_ppm->__set('fn_general', $fn_general);
+    $fn_ppm->__set('fn_task', $fn_task);
+    $fn_ppm->__set('fn_email', $fn_email);
+
+    Class_db::getInstance()->db_connect();
+
+    echo '-----------------<br/>';
+    $assets = Class_db::getInstance()->db_select('z_jai_batch_ppm', array());
+    foreach ($assets as $asset) {
+        //var_dump($asset);
+        Class_db::getInstance()->db_beginTransaction();
+        $result = $fn_ppm->assign_ppm_single($asset['asset_id'], $asset['checklist_id'], $asset['start_date'], '1', $asset['ppm_group_id']);
+        Class_db::getInstance()->db_delete('z_jai_batch_ppm', array('jai_id'=>$asset['jai_id']));
+        Class_db::getInstance()->db_commit();
+        echo '<br/>ppmId = '.$result['ppmId'].', ppmTaskNo = '.$result['ppmTaskNo'];
+        echo '<br/>-----------------<br/>';
+    }
+
+    Class_db::getInstance()->db_close();
+} catch (Exception $ex) {
+    Class_db::getInstance()->db_rollback();
+    Class_db::getInstance()->db_close();
+    $fn_general->log_error('API', $api_name, __LINE__, $ex->getMessage());
+}
