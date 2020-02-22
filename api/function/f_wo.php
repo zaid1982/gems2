f010f5b201bfad790c0fceed006da50ed2e88613
zaid1982 <hemppok.kembong@gmail.com>
2020-02-22 11:40:56 +0800

    Fix work request dashboard list colum WO

--- api/function/f_wo.php
+++ api/function/f_wo.php
@@ -529,7 +529,7 @@ class Class_wo {
             $dataLocal = Class_db::getInstance()->db_select_single('wo_task', array('wo_task_id'=>$this->woTaskId), null, 1);
             $createdBy = $dataLocal['wo_task_created_by'];
             $result['woTaskId'] = $dataLocal['wo_task_id'];
-            $result['woTaskNo'] = $dataLocal['wo_task_wr_confirm'] === '1' ? $dataLocal['wo_task_no'] : '-';
+			$result['woTaskNo'] = $dataLocal['wo_task_is_wr'] === '1' && $dataLocal['wo_task_wr_confirm'] !== '1' ? '-' : $dataLocal['wo_task_no'];
             $result['woTaskRequestNo'] = $this->fn_general->clear_null($dataLocal['wo_task_request_no'], '-');
             $result['woTaskReportedBy'] = $arrUserFullName[intval($createdBy)];
             $result['woTaskTimeResponded'] = str_replace('-', '/', $this->fn_general->clear_null($dataLocal['wo_task_time_responded']));
@@ -1491,7 +1491,7 @@ class Class_wo {
             $arr_dataLocal = Class_db::getInstance()->db_select('wo_task', $arrWhere);
             foreach ($arr_dataLocal as $dataLocal) {
                 $row_result['woTaskId'] = $dataLocal['wo_task_id'];
-                $row_result['woTaskNo'] = $dataLocal['wo_task_wr_confirm'] === '1' ? $dataLocal['wo_task_no'] : '-';
+                $row_result['woTaskNo'] = $dataLocal['wo_task_is_wr'] === '1' && $dataLocal['wo_task_wr_confirm'] !== '1' ? '-' : $dataLocal['wo_task_no'];
                 $row_result['woTaskRequestNo'] = $this->fn_general->clear_null($dataLocal['wo_task_request_no'], '-');
                 $row_result['woTaskType'] = $this->fn_general->clear_null($dataLocal['wo_task_type'], '0');
                 $row_result['woTaskTypeDesc'] = $arrWoType[intval($this->fn_general->clear_null($dataLocal['wo_task_type'], '0'))];
