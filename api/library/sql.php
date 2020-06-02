7dfdda98124eed27f58a5a2cb9fda4dd5bd2a096
zaid1982 <hemppok.kembong@gmail.com>
2020-06-03 03:22:32 +0800

    Fix duplicate ppm task when have multiple frequency

--- api/library/sql.php
+++ api/library/sql.php
@@ -234,7 +234,7 @@ class Class_sql
                 LEFT JOIN ref_status ON ref_status.status_id = ppm_task.ppm_task_status
                 LEFT JOIN sys_user ON sys_user.user_id = ppm_task.ppm_task_assigned_to
                 WHERE wfl_transaction.flow_id = 1 AND wfl_task.task_current = 1 AND ppm_task_start_date >= CURDATE() - INTERVAL 2 MONTH AND ppm_task_start_date <= CURDATE() + INTERVAL 1 MONTH 
-                AND (task_claimed_user = [user_id] OR (task_claimed_user IS NULL AND (wfl_task.checkpoint_id <> 1 OR (wfl_task.checkpoint_id = 1 AND ppm_group_user.user_id = [user_id])) )) [rest_filter]";
+                AND (task_claimed_user = [user_id] OR (task_claimed_user IS NULL AND (wfl_task.checkpoint_id <> 1 OR (wfl_task.checkpoint_id = 1 AND ppm_group_user.user_id = [user_id])) )) [rest_filter] GROUP BY ppm_task.ppm_task_id";
             } else if ($title === 'mw_task_ppm_all') {
                 $sql = "SELECT
                     ppm_task.*,
